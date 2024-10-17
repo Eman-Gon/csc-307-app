ca0de75429e8435003ae646494309dd977a815f3
Eman-Gon <emanpiece@gmail.com>
2024-10-16 22:41:05 -0700

    Add POST request functionality for adding users and update frontend state synchronization

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,9 +1,75 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import Table from "./Table";
+// import Form from "./Form";
+
+// function MyApp() {
+//   const [characters, setCharacters] = useState([]);
+
+//   function removeOneCharacter(index) {
+//     const updated = characters.filter((character, i) => i !== index);
+//     setCharacters(updated);
+//   }
+
+//   function handleSubmit(newPerson) {
+//     setCharacters([...characters, newPerson]); 
+//   }
+
+//   return (
+//     <div className="container">
+//       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+//       <Form handleSubmit={handleSubmit} />
+//     </div>
+//   );
+// }
+
+// export default MyApp;
+
+
+
+
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  // Function to fetch users from backend
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  // Use effect to fetch users on component mount
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // Function to post a new user to backend
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+    return promise;
+  }
+
+  // Function to add a new user to the list
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => i !== index);
@@ -11,7 +77,7 @@ function MyApp() {
   }
 
   function handleSubmit(newPerson) {
-    setCharacters([...characters, newPerson]); 
+    updateList(newPerson);
   }
 
   return (
